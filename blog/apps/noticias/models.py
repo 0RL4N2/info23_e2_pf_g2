48cc3c01a681faabdd2df52325b86d1f4235fc23
0RL4N2 <ramonorland@gmail.com>
2023-08-04 03:40:37 -0300

    vinculando app noticias con base de datos

--- blog/apps/noticias/models.py
+++ blog/apps/noticias/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre= models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=30)
+    resumen = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_de_publicacion = models.DateTimeField(auto_now_add=True)
+    #para imagen debemos instalar pillow
+    imagen = models.ImageField(upload_to= 'noticias')
+    categoria_noticia = models.ForeignKey(Categoria, on_delete= models.CASCADE) #SET_NULL
+    
+    
+    def __str__(self):
+        return self.titulo
