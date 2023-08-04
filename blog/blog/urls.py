6df2e1487c07d1fad33cd6fad7e38c59b82905cd
0RL4N2 <ramonorland@gmail.com>
2023-08-04 00:37:02 -0300

    agregando app noticias para guardar las recetas

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -26,4 +26,7 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('nosotros/', views.Nosotros, name='nosotros'),
     path('cocina/', views.Cocina, name='cocina'),
+
+    #urls aplicaciones
+    path('noticias/', include('apps.noticias.urls')),
 ]
