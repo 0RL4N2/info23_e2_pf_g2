03364a07d7999d10a3a83e9d97243534edea720b
0RL4N2 <ramonorland@gmail.com>
2023-08-06 01:14:07 -0300

    agregando vista template noticias, filtrado por categorias, y comentarios

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,4 @@ urlpatterns = [
     #urls aplicaciones
     path('noticias/', include('apps.noticias.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
